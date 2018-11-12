53973667d2241ae439019f5110e97dc8f4e62387
solcot <solcot@nate.com>
2018-11-12 16:15:56 +0900

    pg connect test

--- DANCER2/lib/DANCER2.pm
+++ DANCER2/lib/DANCER2.pm
@@ -1,10 +1,34 @@
 package DANCER2;
 use Dancer2;
+use DBI;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'DANCER2' };
+	template 'index' => { 'title' => 'DANCER2' };
+};
+
+get '/pgtest' => sub {
+	my $host = config->{host};
+	my $port = config->{port};
+	my $dbname = config->{dbname};
+	my $username = config->{username};
+	my $password = config->{password};
+	
+	my $kk = "dbi:Pg:dbname=$dbname;host=$host;port=$port";
+	#return $kk;
+	
+	my $dbh = DBI -> connect("dbi:Pg:dbname=$dbname;host=$host;port=$port",  
+	                            $username,
+	                            $password,
+	                            {AutoCommit => 0, RaiseError => 1}
+	                         ) or die $DBI::errstr;
+	
+	my $sth = $dbh->prepare("SELECT * FROM book");
+	$sth->execute();                        # execute the query
+	my @row;
+	@row = $sth->fetchrow_array;
+	return join(',',@row);
 };
 
 true;
