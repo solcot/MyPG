0dfce649fd5ec68d9ad5c387a5d3321c23d2786b
solcot <solcot@nate.com>
2018-12-10 15:38:39 +0900

    myboard insert mod2

--- DANCER2/lib/DANCER2.pm
+++ DANCER2/lib/DANCER2.pm
@@ -1,6 +1,7 @@
 package DANCER2;
 use Dancer2;
 use DBI;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
@@ -26,6 +27,7 @@ post '/myboard/insert' => sub {
 	insert into myboard(b_name,b_email,b_title,b_url,b_pwd,b_readnum,b_date,b_ipaddr,b_content)
 	values (?,?,?,?,?,0,current_timestamp,?,?)
 	/;
+print "**** $name $mail $title **** $sql\n\n\n\n";
 	my $sth = $dbh->prepare($sql);
 	$sth->bind_param(1,$name);
 	$sth->bind_param(2,$mail);
@@ -38,6 +40,38 @@ post '/myboard/insert' => sub {
 
 	$sth->finish;
 	$dbh->disconnect;
+};
+
+get '/myboard/list' => sub {
+	my $pagesize = 2;
+	my $gotopage = 1;
+	my $maxnum = $gotopage*$pagesize;
+	my $minnum = ($gotopage-1)*$pagesize+1;
+
+	my $sql = qq/
+	select *
+	from (select a.*, row_number() over(order by board_idx desc) as num
+		from myboard a fetch first $maxnum rows only) a
+	where num >= $minnum
+	/;
+
+	my $dbh = &connect_db;
+	my $sth = $dbh->prepare($sql);
+	$sth->execute();
+	my $vars1 = $sth->fetchall_arrayref([]);
+	#print Dumper($vars1);
+	$sth->finish;
+	$dbh->disconnect;
+	
+	template 'list' => { 'vars1'=>$vars1 }, { 'layout'=>'myboard' };
+
+
+};
+
+get '/myboard/content' => sub {
+	my $board_idx = params->{'board_idx'};
+	return $board_idx;
+
 };
 
 
@@ -59,9 +93,22 @@ sub connect_db {
 
 
 get '/test' => sub {
-	my $kk = request->remote_address;
-	return $kk;
+	my $vars1 = {
+	    article => 'The Third Shoe',
+	    person  => { 
+	        id    => 314, 
+	        name  => 'Mr. Blue',
+	        email => 'blue@nowhere.org',
+	    },
+	    primes  => [ 2, 3, 5, 7, 11, 13 ],
+	    wizard  => sub { return join('---', 'Abracadabra!', @_) },
+	    #cgi     => CGI->new('mode=submit&debug=1'),
+	};
+
+	template 'test'=> { 'vars1'=>$vars1, 'var2'=>'variable2' }, {'layout'=>'myboard'};
+
 };
+
 get '/pgtest' => sub {
 	my $host = config->{host};
 	my $port = config->{port};
