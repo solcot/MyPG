57612470d3ea827b2b85e3685bd74a23a9819081
solcot <44831818+solcot@users.noreply.github.com>
2025-07-19 18:16:32 +0900

    Update KoreaStockAutoTrade.py

--- Stock/KoreaStockAutoTrade.py
+++ Stock/KoreaStockAutoTrade.py
@@ -195,10 +195,10 @@ def get_all_symbols():
     #print("\n✅ 예시 종목코드:", symbols[:5])
     #return symbols
 
-
     # 기존 필터 이후 추가
     #filtered['점수'] = filtered['전일변동폭비율'] * filtered['거래대금']   # 전일에 가격도 크게 움직이고, 돈도 많이 몰린 종목을 추리기 위해
     filtered['점수'] = filtered['전일변동폭비율'] * filtered['거래대금'] * (1 + filtered['등락률'] / 100)
+    #filtered['점수'] = filtered['전일변동폭비율'] * filtered['거래대금'] * (1 + max(0, filtered['등락률'] / 100)) # 음수 등락률은 1로 고정, 하락 종목의 점수 감소 최소화
 
     # 점수 기준 정렬 → 상위 150개 추출
     #top_filtered = filtered.sort_values(by='점수', ascending=False).head(150)
